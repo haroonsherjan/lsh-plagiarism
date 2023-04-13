2df85f8843f270f95fb89b8b5450823ecdfeadd7
Haroon Sherjan <haroonsherjan@gmail.com>
2023-04-12 20:35:37 -0400

    Basic cleaning

--- clean/main.py
+++ clean/main.py
@@ -2,4 +2,7 @@ from pandas import DataFrame
 
 
 def clean_data(data: DataFrame) -> DataFrame:
+    data['text'].replace('(\n|_|\t|\.|\,)', ' ', regex=True, inplace=True)
+    data['text'].replace('[ ][ ][ ]*', '', regex=True, inplace=True)
+    data['text'].str.lower()
     return data
